6249458206a82c7da04eb7b5079ce323bfcb1ec5
tomcollins2110 <tom206collins@gmail.com>
2022-09-19 07:07:00 +0000

    Added second java part

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function darkmode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
+let addToDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
