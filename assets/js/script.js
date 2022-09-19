77a92a31dbbea8db6eb88776584e4bc35b444d8a
tomcollins2110 <tom206collins@gmail.com>
2022-09-19 07:08:20 +0000

    Re-commit for finished java script

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,32 @@ function darkmode() {
 let addToDoButton = document.getElementById('addToDo');
 let toDoContainer = document.getElementById('toDoContainer');
 let inputField = document.getElementById('inputField');
+
+addToDoButton.addEventListener('click', function(){
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = inputField.value;
+    toDoContainer.appendChild(paragraph);
+    inputField.value = "";
+    paragraph.addEventListener('dblclick',function(){
+        toDoContainer.removeChild(paragraph);
+    });
+});
+
+var button = document.getElementById("myButton");
+var number = document.getElementById("number");
+
+number.innerHTML = "0";
+
+var min = 1;
+var max = 7;
+
+function generateNumber() {
+    var random = Math.floor((Math.random() * (max-min) + min ));
+    return random;
+}
+ 
+button.addEventListener("click", function (){
+    var result = generateNumber();
+    number.innerHTML = result;
+});
